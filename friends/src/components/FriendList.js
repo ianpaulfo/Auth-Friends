005d3dbe8fb42678ade14d624501ba739fe8d801
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-04-28 18:01:21 -0700

    set up friendsList

---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import FriendCard from "./FriendCard";
+import FriendForm from "./FriendForm";
+
+const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("friends")
+      .then(res => {
+        console.log(res);
+        setFriends(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h3>Add a Friend</h3>
+      <FriendForm />
+      {friends.map(friend => (
+        <FriendCard key={friend.id} friend={friend} />
+      ))}
+    </div>
+  );
+};
+export default FriendsList;
